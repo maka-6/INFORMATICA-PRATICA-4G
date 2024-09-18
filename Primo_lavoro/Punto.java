da6aef4e09fe390af63f1840c678504ae526bd52
maka-6 <makaouiyouness30@gmail.com>
2024-09-18 10:32:52 +0200

    aggiunta lavoro

---
+++ Primo_lavoro/Punto.java
@@ -0,0 +1,19 @@
+package Primo_lavoro;
+
+public class Punto {
+    private float x;
+    private float y;
+    // commento
+    Punto(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    // costruttore
+    // getter
+    float getX() {
+        return this.x;
+    }
+    float getY() {
+        return this.y;
+    }
+}
---
+++ Primo_lavoro/TestPunto.java
@@ -0,0 +1,8 @@
+package Primo_lavoro;
+
+public class TestPunto {
+    public static void main(String[] args) {
+        Punto punto = new Punto( 3, 4);
+        System.out.println("X: " + punto.getX() + "Y: " + punto.getY());
+    }
+}
