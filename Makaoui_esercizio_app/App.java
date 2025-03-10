76e59d4f29a70522d72e67a26b3aa2a75dba309f
Youness makaoui <119128296+maka-6@users.noreply.github.com>
2025-03-10 13:29:28 +0100

    aggiunta file

---
+++ Makaoui_esercizio_app/App.java
@@ -0,0 +1,8 @@
+package Makaoui_esercizio_app;
+
+public class App {
+    public static void main(String[] args) {
+        MainFrame app = new MainFrame();
+        app.setVisible(true);
+    }
+}
---
+++ Makaoui_esercizio_app/MainFrame.java
@@ -0,0 +1,27 @@
+package Makaoui_esercizio_app;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class MainFrame extends JFrame {
+    JLabel topPanel, midPanel, bottomPanel;
+    JTextField textField;
+
+    public MainFrame() {
+
+        setLayout(new BorderLayout());
+        setTitle("Makaoui Esercizio App");
+        setBounds(100, 100, 450, 300);
+        textField = new JTextField();
+        textField.setEditable(true);
+        textField.setColumns(10);
+        textField.selectAll();
+        topPanel = new JLabel();
+        midPanel = new JLabel();
+        bottomPanel = new JLabel();
+        setSize(450, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
