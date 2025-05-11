5ec2fb9117d3eb2d25b50e14ab21db74870541d6
Youness makaoui <119128296+maka-6@users.noreply.github.com>
2025-05-12 00:26:52 +0200

    aggiornamento file

--- jaylib-example-project-maka/src/main/java/Zantrum/Cards/SpecialEffectCard.java
+++
@@ -1,8 +0,0 @@
-package Zantrum.Cards;
-
-public class SpecialEffectCard extends Card {
-
-    public SpecialEffectCard(String name, int life, int number, String description, int rarity, String origin, CardType type) {
-        super(name, life, number, description, rarity, origin, type);
-    }
-}
