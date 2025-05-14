049808d5f8520426a6a05fd22efe7f62a8279588
Youness makaoui <119128296+maka-6@users.noreply.github.com>
2025-05-14 22:17:33 +0200

    aggiunta display calcolatrice organizzazione dei pulsanti layout calcolatrice riordinato

--- Lavoro_gruppi_Calcolatrice_Scientifica_Grafica/src/CalcolatriceScientifica2.java
+++ Lavoro_gruppi_Calcolatrice_Scientifica_Grafica/src/CalcolatriceScientifica2.java
@@ -8,29 +8,38 @@ import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 
 public class CalcolatriceScientifica2 extends JFrame {
 
+    //
     private JTextArea outputArea;
     private OperazioniMatematiche2 operazioniMatematiche2;
 
-    // modalita di risoluzione
-    private boolean mode; // true scientifica; false grafica
+    // modalità di risoluzione
+    public boolean mode; // true scientifica; false grafica
 
+    // display della calcolatrice
     private DisplayGrafico displayGrafico;
-    //
+
+    // componenti espressione
     private String expression;
     private ArrayList<Function> functions;
-    private ArrayList<Char> operators;
+    private ArrayList<String> operators;
 
+    // la formula in formato icona con laTex
     private TeXFormula formula;
     private TeXIcon icon;
+
+    //
     JLabel latexLabel;
     JPanel latexPanel;
 
     public CalcolatriceScientifica2() {
+
+        mode = true;
 
         setTitle("Calcolatrice Scientifica Grafica di Makaoui e Zennaro");
         setSize(600, 800);
@@ -53,52 +62,120 @@ public class CalcolatriceScientifica2 extends JFrame {
         add(latexPanel, BorderLayout.NORTH); // O dove preferisci nel layout
 
         operazioniMatematiche2 = new OperazioniMatematiche2();
+        displayGrafico = new DisplayGrafico(600, 400);
+        GraficoPanel graficoPanel = new GraficoPanel(displayGrafico);
+        add(graficoPanel, BorderLayout.CENTER);
 
-        JPanel buttonPanel = createButtonPanel();
+        JPanel buttonFunctionPanel = createFunctionPanel();
+        JPanel buttonNumberPanel = createNumberPanel();
+        JPanel buttonOperatorPanel = createOperatorPanel();
 
-        add(new JScrollPane(outputArea), BorderLayout.CENTER);
-        add(buttonPanel, BorderLayout.SOUTH);
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        bottomPanel.add(buttonFunctionPanel, BorderLayout.NORTH);
+        bottomPanel.add(buttonOperatorPanel, BorderLayout.EAST);
+        bottomPanel.add(buttonNumberPanel, BorderLayout.CENTER);
+
+        add(bottomPanel, BorderLayout.SOUTH);
+
+        //add(new JScrollPane(outputArea), BorderLayout.CENTER);
 
         expression = "";
         functions = new ArrayList<>();
+        operators = new ArrayList<>();
     }
 
-    private JPanel createButtonPanel() {
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(5, 4, 10, 10));
-        buttonPanel.setBackground(new Color(128, 128, 128));
+    private JPanel createFunctionPanel() {
+        JPanel panel = new JPanel(new GridLayout(4, 4, 5, 5));
+        panel.setBackground(new Color(128, 128, 128));
 
-        String[] buttons = {"x"+"\u1D3A","\u1D3A"+"√x", "Log", "+",
-                            "Sin", "Cos", "Tan", "-",
-                            "Ln", "Cot", "ArcCot", "/",
-                            "ArcSin", "ArcCos", "Arctan", "*",
-                            "n!", "Multiply for π", "Clear", "=",
-                            "Graphic", "Scientific"};
+        String[] functions = {
+                "x","x"+"\u1D3A", "\u1D3A"+"√x", "Log", "Ln",
+                "Sin", "Cos", "Tan", "Cot",
+                "n!", "π", "Graphic",
+                "Scientific", "Clear"
+        };
 
-        for (String button : buttons) {
-            JButton b = new JButton(button);
-            b.setFont(new Font("Arial", Font.BOLD, 12));
+        for (String text : functions) {
+            JButton b = new JButton(text);
+            b.setFont(new Font("Arial", Font.PLAIN, 10)); // ridotto
+            b.setPreferredSize(new Dimension(20, 20)); // Imposta dimensioni preferite
             b.setBackground(new Color(192, 192, 192));
             b.setForeground(Color.BLACK);
             b.setFocusPainted(false);
             b.addActionListener(new ButtonListener());
-            buttonPanel.add(b);
+            panel.add(b);
         }
 
-        return buttonPanel;
+        return panel;
+    }
+
+    private JPanel createOperatorPanel() {
+        JPanel panel = new JPanel(new GridLayout(5, 1, 5, 5));
+        panel.setBackground(new Color(112, 112, 112));
+
+        String[] operators = {"+", "-", "*", "/", "="};
+
+        for (String text : operators) {
+            JButton b = new JButton(text);
+            b.setFont(new Font("Arial", Font.BOLD, 12));
+            b.setBackground(new Color(200, 200, 200));
+            b.setForeground(Color.BLACK);
+            b.setFocusPainted(false);
+            b.addActionListener(new ButtonListener());
+            panel.add(b);
+        }
+
+        return panel;
+    }
+
+    private JPanel createNumberPanel() {
+        JPanel panel = new JPanel(new GridLayout(5, 1, 5, 5));
+        panel.setBackground(new Color(112, 112, 112));
+
+        String[] operators = {"0", "1", "2", "3", "4","5","6","7","8","9"};
+
+        for (String text : operators) {
+            JButton b = new JButton(text);
+            b.setFont(new Font("Arial", Font.BOLD, 12));
+            b.setBackground(new Color(200, 200, 200));
+            b.setPreferredSize(new Dimension(5, 20)); // Imposta dimensioni preferite
+            b.setForeground(Color.BLACK);
+            b.setFocusPainted(false);
+            b.addActionListener(new ButtonListener());
+            panel.add(b);
+        }
+
+        return panel;
     }
 
     private class ButtonListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
+
+            // stampa
+            if (mode) {
+                outputArea.setText("Mode: Scientific");
+            } else {
+                outputArea.setText("Mode: Graphic");
+            }
+
             String command = e.getActionCommand();
+
             try {
-                expression = operazioniMatematiche2.buildExpression(command, functions, expression);
+                // Se il comando è "Graphic" o "Scientific"
+                if (command.equals("Graphic")) {
+                    mode = false;
+                    return;
+                } else if (command.equals("Scientific")) {
+                    mode = true;
+                    return;
+                }
+
+                expression = operazioniMatematiche2.buildExpression(command, functions, expression, operators, mode);
                 TeXFormula formula = new TeXFormula(expression);
                 TeXIcon icon = formula.createTeXIcon(TeXFormula.SERIF, 20);
                 latexLabel.setIcon(icon);
-                latexLabel.repaint(); // forza aggiornamento
-                System.out.println(functions.toString());
+                latexLabel.repaint();
             } catch (Exception ex) {
                 JOptionPane.showMessageDialog(null, ex.getMessage(), "Errore", JOptionPane.ERROR_MESSAGE);
             }
@@ -112,26 +189,75 @@ public class CalcolatriceScientifica2 extends JFrame {
 
 class OperazioniMatematiche2 {
 
-    public String buildExpression(String operazione, ArrayList<Function> functions, String expression) {
-        double num1, num2;
+    // costruisco l'espressione
+    public String buildExpression(String operazione, ArrayList<Function> functions, String expression, ArrayList<String> operators, boolean mode) {
+
+        // primo controllo
+        if (mode) { // modalità scientifica
+            if (operazione.contains("x") || operazione.contains("X")) {
+                throw new IllegalArgumentException("La variabile x è disponibile solo in modalità grafica.");
+            }
+        }
+
+
+        String num1, num2;
         String[] arguments;
 
         switch (operazione) {
 
+            case "0":
+                expression += "0";
+                break;
+            case "1":
+                expression += "1";
+                break;
+            case "2":
+                expression += "2";
+                break;
+            case "3":
+                expression += "3";
+                break;
+            case "4":
+                expression += "4";
+                break;
+            case "5":
+                expression += "5";
+                break;
+            case "6":
+                expression += "6";
+                break;
+            case "7":
+                expression += "7";
+                break;
+            case "8":
+                expression += "8";
+                break;
+            case "9":
+                expression += "9";
+                break;
+
             case "+":
                 expression += " + ";
+                operators.add("+");
                 break;
 
             case "-":
                 expression += " - ";
+                operators.add("-");
                 break;
 
             case "*":
                 expression += " * ";
+                operators.add("*");
                 break;
 
             case "/":
                 expression += " / ";
+                operators.add("/");
+                break;
+
+            case "x":
+                expression += " x";
                 break;
 
             case "x"+"\u1D3A":
@@ -141,23 +267,23 @@ class OperazioniMatematiche2 {
                 expression += num1 + "^" + num2;
                 break;
 
-            case "\u1D3C"+"√x":
+            case "\u1D3A"+"√x":
                 num1 = getInput("Inserisci N > 0");
                 num2 = getInput("Inserisci X ");
-                if (num1 < 0 && num2 % 2 == 0) throw new ArithmeticException("Errore: radice pari di X < 0");
+                if (Integer.parseInt(num1) < 0 && Integer.parseInt(num2) % 2 == 0) throw new ArithmeticException("Errore: radice pari di X < 0");
 
-                expression += "\u1D3A" + num1 + num2;
+                expression += num1 + "\\sqrt{" + "}" + num2;
                 break;
 
             case "Log":
-                num1 = getInput("Inserisci N");
-                num2 = getInput("Inserisci a");
-                if (num1 <= 0 || num2 <= 0 || num2 == 1) throw new ArithmeticException("Errore: a > 1 e X > 0");
+                num1 = getInput("Inserisci N:");
+                num2 = getInput("Inserisci A:");
+                if (Integer.parseInt(num1) <= 0 || Integer.parseInt(num2) <= 0 || Integer.parseInt(num2) == 1) throw new ArithmeticException("Errore: A != 0 e A > 0 e X > 0");
 
                 expression += "log_{"+ num2 + "}{" + num1 + "}";
                 arguments = new String[2];
-                arguments[0] = Double.toString(num1);
-                arguments[1] = Double.toString(num2);
+                arguments[0] = num1;
+                arguments[1] = num2;
 
                 Function Log = new Function("log",arguments);
                 functions.add(Log);
@@ -165,11 +291,11 @@ class OperazioniMatematiche2 {
 
             case "Ln":
                 num1 = getInput("Inserisci N > 0");
-                if (num1 <= 0) throw new ArithmeticException("Errore: Ln è definito solo per N > 0");
+                if (Integer.parseInt(num1) <= 0) throw new ArithmeticException("Errore: Ln è definito solo per N > 0");
 
                 expression += "ln{" + num1 + "}";
                 arguments = new String[1];
-                arguments[0] = Double.toString(num1);
+                arguments[0] = num1;
                 Function Ln = new Function("Ln",arguments);
                 functions.add(Ln);
                 break;
@@ -180,7 +306,7 @@ class OperazioniMatematiche2 {
                 expression += "Sin(" + num1 + ")";
 
                 arguments = new String[1];
-                arguments[0] = Double.toString(num1);
+                arguments[0] = num1;
                 Function Sin = new Function("Sin",arguments);
                 functions.add(Sin);
                 break;
@@ -190,7 +316,7 @@ class OperazioniMatematiche2 {
 
                 expression += "Cos(" + num1 + ")";
                 arguments = new String[1];
-                arguments[0] = Double.toString(num1);
+                arguments[0] = num1;
                 Function Cos = new Function("Cos",arguments);
                 functions.add(Cos);
                 break;
@@ -200,15 +326,35 @@ class OperazioniMatematiche2 {
 
                 expression += "Tan(" + num1 + ")";
                 arguments = new String[1];
-                arguments[0] = Double.toString(num1);
+                arguments[0] = num1;
                 Function Tan = new Function("tan",arguments);
                 functions.add(Tan);
                 break;
 
+            case "Cot":
+                num1 = getInput("Inserisci N:");
+                expression += "Cot(" + num1 + ")";
+                arguments = new String[1];
+                arguments[0] = num1;
+                Function Cot = new Function("Cot",arguments);
+                functions.add(Cot);
+                break;
+
             case "n!":
-                num1 = (int) getInput("Inserisci N > 0 e intero:");
-                if (num1 < 0) throw new ArithmeticException("Errore: N! con N < 0");
+                num1 = getInput("Inserisci N > 0 e intero:");
+                if (Integer.parseInt(num1) < 0) throw new ArithmeticException("Errore: N! con N < 0");
                 expression += num1 + "!";
+                break;
+
+            // pulisco la calcolatrice
+            case "Clear":
+                expression = "";
+                functions.clear();
+                operators.clear();
+                break;
+
+            case "=":
+                //Lestgoski();
                 break;
 
             default:
@@ -218,12 +364,18 @@ class OperazioniMatematiche2 {
         return expression;
     }
 
-    private double getInput(String message) throws NumberFormatException {
+    // verifico l'espressione
+    public boolean verifyExpression(ArrayList<String> operators, ArrayList<String> arguments) {
+
+        return true;
+    }
+
+    private String getInput(String message) throws NumberFormatException {
         String input = JOptionPane.showInputDialog(message);
         if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException("Input non valido!");
         }
-        return Double.parseDouble(input);
+        return input;
     }
 }
 
@@ -249,5 +401,20 @@ class Function {
     @Override
     public String toString() {
         return name + "(" + argument[0] + ")";
+    }
+}
+
+class GraficoPanel extends JPanel {
+    private DisplayGrafico display;
+
+    public GraficoPanel(DisplayGrafico display) {
+        this.display = display;
+        setPreferredSize(new Dimension(600, 400)); // oppure adatta alle tue misure
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(display.getImage(), 0, 0, null);
     }
 }
--- Lavoro_gruppi_Calcolatrice_Scientifica_Grafica/src/DisplayGrafico.java
+++ Lavoro_gruppi_Calcolatrice_Scientifica_Grafica/src/DisplayGrafico.java
@@ -7,14 +7,33 @@ public class DisplayGrafico {
     private BufferedImage image;
     private Graphics2D g2d;
 
-    DisplayGrafico(int x, int y) {
-        image = new BufferedImage(x, y, BufferedImage.TYPE_INT_RGB);
+    DisplayGrafico(int width, int height) {
+        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         // Imposta tutti i pixel a nero (o spenti)
         g2d = image.createGraphics();
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(0, 0, width, height);
 
-        g2d.setColor(Color.RED);
-        g2d.fillRect(0, 0, 600, 400);
-        g2d.dispose();
+        g2d.setColor(Color.LIGHT_GRAY);
+        for (int x = 0; x < width; x += 50) {
+            g2d.drawLine(x, 0, x, height);
+        }
+        for (int y = 0; y < height; y += 50) {
+            g2d.drawLine(0, y, width, y);
+        }
+
+        g2d.setColor(Color.BLACK);
+        // asse x
+        g2d.drawLine(0, height/2, width, height/2);
+        // asse y
+        g2d.drawLine(width/2, 0, width/2, height);
+
+        //g2d.dispose();
+    }
+
+    // Metodo pubblico per ottenere l'immagine
+    public BufferedImage getImage() {
+        return image;
     }
 
     public void Draw (int x, int y, Color color) {
